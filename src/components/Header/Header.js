fbe516fd3205dff5d546a637b8b65740ea0a61f8
Waldyn Benbenek <wben1201@gmail.com>
2019-02-21 15:01:53 -0600

    Header done.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Header.css';
+class Header extends Component {
+    render() {
+        return (
+            <div className="colorH">
+                <h1>React Famous People</h1>
+            </div>
+        )
+    }
+}
+export default Header
